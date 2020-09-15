6af009571b3b3e7f4ad533091837e50c8aa384ba
andrea <andrea@localhost.localdomain>
2020-09-15 09:25:05 +0200

    added gnome pop shell

--- gnome-restore.sh
+++ gnome-restore.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
+
+dnf update
+
 mkdir ~/Pictures/theme-images 
 cp ./myDesktop/desktop.jpg ~/Pictures/theme-images/desktop.jpg
 cp ./myDesktop/lock.jpg ~/Pictures/theme-images/lock.jpg
 dconf load -f /org/gnome/ < ./myDesktop/my_gnome_settings
+
+cd ..
+
+sudo dnf install make
+git clone https://github.com/pop-os/shell
+clear
+cd shell
+yes | ./rebuild.sh
+
+cd ..
